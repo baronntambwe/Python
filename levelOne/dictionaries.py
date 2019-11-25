fc76dfefb7faa517d397e276e35eaf84bce54943
Baron Ntambwe <baronbabutuke@gmail.com>
2019-11-25 12:03:37 -0500

    Added a new file

---
+++ levelOne/dictionaries.py
@@ -0,0 +1,7 @@
+
+
+# dictionaries
+
+my_stuffs = {"key1":"value1", "key2":2, "key3":True, "key4":{123:[1,2,"Grabe this"]}}
+
+print(my_stuffs["key4"][123][2])
---
+++ levelOne/tuples_sets.py
@@ -0,0 +1,7 @@
+
+
+# tuples
+
+t = (1,2,True,"Me")
+
+print(t[2])
