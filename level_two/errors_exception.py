2cc65097650396bbb34945ee28fb5335617e994c
Baron Ntambwe <baronbabutuke@gmail.com>
2019-11-29 01:11:01 -0500

    New file

--- level_two/errors_exception.py
+++ level_two/errors_exception.py
@@ -1,3 +1,9 @@
-# CLASS OBJECT ATTRIBUTE
 
- 
+try:
+    fs = open("simple.txt","r")
+    fs.write("Hello text")
+except IOError:
+    print("Could not find the file")
+else:
+    print("Success!")
+    fs.close()
