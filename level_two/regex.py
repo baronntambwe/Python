d1ea5b69026667daba1bd6dcedec0f56658df96a
Baron Ntambwe <baronbabutuke@gmail.com>
2019-11-30 02:52:27 -0500

    New file

--- level_two/regex.py
+++ level_two/regex.py
@@ -9,6 +9,12 @@ def multi_re_find(patterns, phrase):
 
 test_phrase = "sddsd...sddsd....sddsd...sdddssddd....sdddsd...sddsddd"
 test_pattern = ['sd*'] # 0 or more d
-test_pattern = ['sd+'] # 0 or more d
+test_pattern = ['sd+'] # 1 or more d
+test_pattern = ['sd?'] # 0 or 1 d
+test_pattern = ['sd{4}'] # 4 d
+test_pattern = ['sd{2,4}'] # 2 or 4 d
+test_pattern = ['sd[sd]+'] # 1 or more s or d
+
+test_pattern = ['[^!@&]+'] # Remove 1 or more !, @ and &
 
 multi_re_find(test_pattern,test_phrase)
