396d32d998ddf302a17534d654d988ebcd00f59f
Baron Ntambwe <baronbabutuke@gmail.com>
2020-01-28 06:50:43 -0500

    Fixed linty installation

--- django/first_project/populate_first_app.py
+++ django/first_project/populate_first_app.py
@@ -16,6 +16,34 @@ topics = ['Social', 'Search', 'Marketplace', 'Games', 'News']
 def add_topic():
     t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]    # If it does not exist, it creates
     t.save()
-
     return t
+
+def populate(N=5):
+
+   
+    for entry in range(N):
+
+            # get the Topic for the entry
+            top = add_topic()
+
+            # create the fake data for that entry
+            fake_url = fakegen.url()
+            fake_date = fakegen.date()
+            fake_name = fakegen.company()
+
+            # create a new webpage entry
+            webpg = WebPage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+
+            # create a fake access record for that webpage
+            acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
+
+if __name__ == '__main__':
+    print('Populating fake data')
+    populate(20)
+    print('Population complete!')
+
+
+    
+
+
 
